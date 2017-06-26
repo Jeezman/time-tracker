7d60654adb73814cce1207c226d6ae06520ebd46
Olutobi Adeyemi <tobiadeyemi91@gmail.com>
2017-06-26 23:59:51 +0100

    added index.html file added index js file and TimerDashboard component

---
+++ public/js/app.js
@@ -0,0 +1,12 @@
+class TimerDashboard extends React.Component {
+    render () {
+        return (
+            <div className="ui three column centered grid">
+                <div className="column">
+                    <EditableTimerList />
+                    <ToggableTimerForm isOpen={true} />
+                </div>
+            </div>
+        )
+    }
+}
