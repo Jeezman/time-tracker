adf8f3a73e99779af13ff66b0a64be5390886314
Olutobi Adeyemi <tobiadeyemi91@gmail.com>
2017-06-28 01:10:59 +0100

    added state to TimerDashboard

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,31 @@
 class TimerDashboard extends React.Component {
+
+    state = {
+        timers: [
+            {
+                title: 'Practice Squat',
+                project: 'Gym Chores',
+                id: uuid.v4(),
+                elapsed: 5456099,
+                runningSince: Date.now(),
+            },
+            {
+                title: 'Bake squash',
+                project: 'Kitchen Chores',
+                id: uuid.v4(),
+                elapsed: 1273998,
+                runningSince: null,
+            },
+        ],
+    }
+
     render () {
         return (
             <div className="ui three column centered grid">
                 <div className="column">
-                    <EditableTimerList />
+                    <EditableTimerList 
+                        timers={this.state.timers}
+                    />
                     <ToggableTimerForm isOpen={false} />
                 </div>
             </div>
