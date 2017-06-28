c3e56729c6f03c2ead5b982b0cea3c420322fe2f
Olutobi Adeyemi <tobiadeyemi91@gmail.com>
2017-06-28 03:43:00 +0100

    modified TimerForm component set up inverse data flow add event handlers

--- public/js/app.js
+++ public/js/app.js
@@ -98,11 +98,19 @@ class TimerForm extends React.Component {
         this.setState({ project: e.target.value });
     };
 
+    handleSubmit = () => {
+        this.props.onFormSubmit({
+            id: this.props.id,
+            title: this.state.title,
+            project: this.state.project
+        })
+    }
+
 
 
     render () {
-        // uses the presence of this.props.title to determine what text the submit button should display
-        const submitText = this.props.title ? 'Update' : 'Create';
+        //use the id property to determine whether or not an object has been created
+        const submitText = this.props.id ? 'Update' : 'Create';
         return (
             <div className="ui centered card">
                 <div className="content">
@@ -124,10 +132,16 @@ class TimerForm extends React.Component {
                             />
                         </div>
                         <div className="ui two bottom attached buttons">
-                            <button className="ui basic blue button">
+                            <button 
+                                className="ui basic blue button"
+                                onClick={this.handleSubmit}
+                            >
                                 {submitText}
                             </button>
-                            <button className="ui basic red button">
+                            <button 
+                                className="ui basic red button"
+                                onClick={this.props.onFormClose}
+                            >
                                 Cancel
                             </button>
                         </div>
