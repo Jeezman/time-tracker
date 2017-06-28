d34e11e9ace1327d61cde36c6aa7f1c5e591e631
Olutobi Adeyemi <tobiadeyemi91@gmail.com>
2017-06-28 02:50:47 +0100

    added state to TimerForm component added functions that can  modify state

--- public/js/app.js
+++ public/js/app.js
@@ -84,6 +84,22 @@ class EditableTimer extends React.Component {
 
 //renders a form for creating a new timer or editing an existing one
 class TimerForm extends React.Component {
+
+    state = {
+        title: this.props.title || '',
+        project: this.props.project || '',
+    };
+
+    handleTitleChange = (e) => {
+        this.setState({ title: e.target.value });
+    };
+
+    handleProjectChange = (e) => {
+        this.setState({ project: e.target.value });
+    };
+
+
+
     render () {
         // uses the presence of this.props.title to determine what text the submit button should display
         const submitText = this.props.title ? 'Update' : 'Create';
@@ -92,12 +108,20 @@ class TimerForm extends React.Component {
                 <div className="content">
                     <div className="ui form">
                         <div className="field">
-                            <label htmlFor="">Title</label>
-                            <input type="text" defaultValue={this.props.title} />
+                            <label>Title</label>
+                            <input 
+                                type="text" 
+                                value={this.state.title} 
+                                onChange={this.handleTitleChange}
+                            />
                         </div>
                         <div className="field">
                             <label htmlFor="">Project</label>
-                            <input type="text" defaultValue={this.props.project}/>
+                            <input 
+                                type="text" 
+                                value={this.state.project}
+                                onChange={this.handleProjectChange}
+                            />
                         </div>
                         <div className="ui two bottom attached buttons">
                             <button className="ui basic blue button">
